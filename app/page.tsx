0e9233d00809ded718f7bd7e4c41a3f79ffe5be2
amthucdanang2015-pixel <amthucdanang2015@gmail.com>
2026-07-04 13:06:28 +0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import Effect from "@/components/Effect";
 import Gallery from "@/components/Gallery";
 import Nav from "@/components/Nav";
+import HIW from "@/components/HIW";
+import Pricing from "@/components/Pricing";
+import Guides from "@/components/Guides";
+import FAQ from "@/components/FAQ";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#111111] text-white">
       <Nav />
       <Gallery />
+      <Effect />
+      <HIW />
+      <Pricing />
+      <FAQ />
+      <Guides />
       <main className="pt-24">
       </main>
     </div>
