4bd141b553cb7b690293ba30c982f273f91286b8
amthucdanang2015-pixel <amthucdanang2015@gmail.com>
2026-07-05 18:39:49 +0700

    Add files via upload

---
+++ app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function SubdomainPage() {
+    return (
+        <div className="flex h-screen w-full items-center justify-center bg-[#090909] text-white">
+            <h1 className="text-4xl font-light tracking-tight">hello world</h1>
+        </div>
+    );
+}
