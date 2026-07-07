38337fd2a383cfd5bc7f3b7fb241632e5cd8d6f0
amthucdanang2015-pixel <amthucdanang2015@gmail.com>
2026-07-07 17:25:37 +0700

    Add files via upload

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,5 @@
+import Editor from "@/components/Editor";
+
 export default function SubdomainPage() {
-    return (
-        <div className="flex h-screen w-full items-center justify-center bg-[#090909] text-white">
-            <h1 className="text-4xl font-light tracking-tight">hello world</h1>
-        </div>
-    );
+    return <Editor />;
 }
