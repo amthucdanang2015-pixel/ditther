3c16167aa502a2e02c3c19d609ad11a43505f023
amthucdanang2015-pixel <amthucdanang2015@gmail.com>
2026-07-11 18:10:01 +0700

    Add files via upload

--- components/LooksGrid.tsx
+++ components/LooksGrid.tsx
@@ -13,14 +13,18 @@ const looks = [
     "strip_8.jpg",
     "strip_9.jpg",
 ];
+const lookTxt = [
+    "Arcade", "Japanese", "Static", "Prism", "Acid Rain", "Mosaic", "Voxel", "Terminal"
+];
+
 
 export default function LooksGrid() {
     return (
-        <div className="grid grid-cols-3 gap-3">
+        <div className="grid grid-cols-3 gap-1.5">
             {looks.map((img, index) => (
                 <button
                     key={img}
-                    className="relative aspect-[1.2] overflow-hidden rounded-2xl group cursor-pointer"
+                    className="relative aspect-[1.5] overflow-hidden rounded-2xl group cursor-pointer"
                 >
                     <Image
                         loading="eager"
@@ -30,6 +34,7 @@ export default function LooksGrid() {
                         alt=""
                         className="object-cover transition duration-300 group-hover:scale-105"
                     />
+                    <p className="absolute bottom-0  text-white text-xs px-2 pb-2">{lookTxt[index]}</p>
                 </button>
             ))}
 
